4bda31e7c55aa48e56af61104e54cca8b0170e67
Arian Motavasseli <amotavasseli@gmail.com>
2018-06-07 16:23:52 -0700

    fixed navigation, total listing, and url

--- ZipApplier/ZipApplier.Console/Program.cs
+++ ZipApplier/ZipApplier.Console/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml;
 
@@ -15,8 +16,7 @@ namespace ZipApplier.ConsoleApp
     {
         static void Main(string[] args)
         {
-            string url = "https://www.ziprecruiter.com/candidate/search?search=.Net+Developer&location=Los+Angeles%2C+CA&days=10&radius=25&refine_by_salary=&refine_by_tags=&refine_by_title=&refine_by_org_name=";
-
+            string url = "https://www.ziprecruiter.com/candidate/search?search=Junior+Software+Developer&location=Los+Angeles%2C+CA&days=10&radius=25";
             ChromeOptions options = new ChromeOptions();
             options.AddArgument("--headless");
             options.AddArgument("--incognito");
@@ -31,41 +31,40 @@ namespace ZipApplier.ConsoleApp
             captureListings(doc);
 
             //Below finds the total number of listings returned by the search.
-            // 1. locate the class that contains the text with the listing. 
+            // 1. locate element that contains the text with the listing. 
             // 2. cut the number out of the string.
             // 3. convert the string into an int. 
-            var headline = doc.QuerySelector("h1.headline").TextContent;
+            var headline = doc.QuerySelector("title").TextContent;
             string stringTotal = headline.Substring(0, headline.IndexOf("+"));
+            if (stringTotal.Contains(","))
+                stringTotal = stringTotal.Replace(",", "");
             int total = Convert.ToInt32(stringTotal);
-
             //ZipRecruiter lists 20 job postings on each page. 
             //Below will determine how many pages need to be checked to work around pagination. 
             int pages = 1;
             int extraPage = 0; 
+
             if(total > 20)
                 pages = (int)Math.Floor((decimal)(total / 20));
+
             if(total % 20 != 0)
-            {
                 extraPage = 1;
-            }
+
             pages += extraPage;
             
             if(pages > 1)
             {
                 for (int p = 2; p <= pages; p++)
                 {
-                    options = new ChromeOptions();
-                    options.AddArgument("--headless");
-                    options.AddArgument("--incognito");
-                    options.AddArgument("--ignore-certificate-errors");
-                    chromeDriver = new ChromeDriver(options);
-                    chromeDriver.Url = url + "&page=" + p;
+                    chromeDriver.Navigate().GoToUrl(url + "&page=" + p);
+                    Console.WriteLine("CURRENT PAGE: {0}", p);
                     html = chromeDriver.PageSource;
                     parser = new HtmlParser();
                     doc = parser.Parse(html);
                     captureListings(doc);
                 }
             }
+            // Inline function will be called to parse html for all necessary data. 
             void captureListings(IHtmlDocument document)
             {
                 var listings = document.QuerySelectorAll("article.t_job_result");
@@ -73,6 +72,7 @@ namespace ZipApplier.ConsoleApp
                 {
                     var listing = listings[i];
                     var title = listing.QuerySelector("h2.job_title").TextContent;
+                    // avoid jobs with the following titles: 
                     if (!title.Contains("Senior")
                         && !title.Contains("Sr")
                         && !title.Contains("Lead")
@@ -95,22 +95,31 @@ namespace ZipApplier.ConsoleApp
                         && !title.Contains("Salesforce")
                         && !title.Contains("SENIOR")
                         && !title.Contains("Analyst")
-                        && title.Contains(".NET") //this needs to be changed with each search
+                        //include jobs that contain the following titles
+                        && title.Contains("Software") //this needs to be changed with each search
                         )
                     {
                         var id = listing.QuerySelector("span.just_job_title").GetAttribute("data-job-id");
+                        Console.WriteLine();
                         Console.WriteLine("Id: " + id);
-                        Console.ReadLine();
+                        Console.WriteLine("Title: " + title);
+                        Console.WriteLine("Company: " + listing.QuerySelector(".t_org_link").TextContent);
+                        if (listing.QuerySelector("button").ClassList.Contains("job_apply"))
+                        {
+                            Console.WriteLine("QUICK APPLY");
+                        }
+                        Console.WriteLine();
+
                         //Job job = new Job();
                         //job.Title = title;
                         //job.Company = listing.QuerySelector(".t_org_link").TextContent;
                         //job.Location = listing.QuerySelector(".t_location_link").TextContent;
                         //job.Description = listing.QuerySelector(".job_snippet").TextContent;
-
-
                     }
                 }
+               
             }
+            Console.ReadLine();
 
         }
     }
--- ZipApplier/ZipApplier.Services/ZipScraper.cs
+++ ZipApplier/ZipApplier.Services/ZipScraper.cs
@@ -31,48 +31,47 @@ namespace ZipApplier.Services
             captureListings(doc);
 
             //Below finds the total number of listings returned by the search.
-            // 1. locate the class that contains the text with the listing. 
+            // 1. locate element that contains the text with the listing. 
             // 2. cut the number out of the string.
             // 3. convert the string into an int. 
-            var headline = doc.QuerySelector("h1.headline").TextContent;
+            var headline = doc.QuerySelector("title").TextContent;
             string stringTotal = headline.Substring(0, headline.IndexOf("+"));
+            if (stringTotal.Contains(","))
+                stringTotal = stringTotal.Replace(",", "");
             int total = Convert.ToInt32(stringTotal);
-
             //ZipRecruiter lists 20 job postings on each page. 
             //Below will determine how many pages need to be checked to work around pagination. 
             int pages = 1;
             int extraPage = 0;
+
             if (total > 20)
                 pages = (int)Math.Floor((decimal)(total / 20));
+
             if (total % 20 != 0)
-            {
                 extraPage = 1;
-            }
+
             pages += extraPage;
 
             if (pages > 1)
             {
                 for (int p = 2; p <= pages; p++)
                 {
-                    options = new ChromeOptions();
-                    options.AddArgument("--headless");
-                    options.AddArgument("--incognito");
-                    options.AddArgument("--ignore-certificate-errors");
-                    chromeDriver = new ChromeDriver("C:\\repos\\github\\ZipApplier\\ZipApplier\\ZipApplier.Services\\bin\\Release", options);
-                    chromeDriver.Url = url + "&page=" + p;
+                    chromeDriver.Navigate().GoToUrl(url + "&page=" + p);
                     html = chromeDriver.PageSource;
                     parser = new HtmlParser();
                     doc = parser.Parse(html);
                     captureListings(doc);
                 }
             }
+            // Inline function will be called to parse html for all necessary data. 
             void captureListings(IHtmlDocument document)
             {
                 var listings = document.QuerySelectorAll("article.t_job_result");
                 for (int i = 0; i < listings.Length; i++)
                 {
                     var listing = listings[i];
-                    var title = listing.QuerySelector("span.just_job_title").TextContent;
+                    var title = listing.QuerySelector("h2.job_title").TextContent;
+                    // avoid jobs with the following titles: 
                     if (!title.Contains("Senior")
                         && !title.Contains("Sr")
                         && !title.Contains("Lead")
@@ -94,32 +93,25 @@ namespace ZipApplier.Services
                         && !title.Contains("CSS")
                         && !title.Contains("Salesforce")
                         && !title.Contains("SENIOR")
-                        && !title.Contains("Analyst") 
-                        // TODO: Setup to ignore case sensitivity 
-                        //this needs to be changed with each unique search
+                        && !title.Contains("Analyst")
+                        //include jobs that contain the following titles
+                        && title.Contains(".NET") //this needs to be changed with each search
                         )
                     {
+                        var id = listing.QuerySelector("span.just_job_title").GetAttribute("data-job-id");
+
                         Job job = new Job();
-                        job.JobId = listing.QuerySelector("span.just_job_title").GetAttribute("data-job-id");
+                        job.JobId = id;
                         job.Title = title;
                         job.Company = listing.QuerySelector(".t_org_link").TextContent;
                         job.Location = listing.QuerySelector(".t_location_link").TextContent;
+                        string link = listing.QuerySelector("a.t_job_link").GetAttribute("href");
+                        link = link.Substring(0, link.IndexOf("?"));
+                        job.Url = link;
                         job.Description = listing.QuerySelector(".job_snippet").TextContent;
-                        if (listing.QuerySelector("button").HasAttribute("data-oneclick"))
-                        {
-                            job.Url = listing.QuerySelector("button").GetAttribute("data-href");
+                        if (listing.QuerySelector("button").ClassList.Contains("job_apply"))
                             job.QuickApply = true;
-                        }
-                        else
-                        {
-                            string  attr = listing.QuerySelector("a.t_job_link").GetAttribute("href");
-                            string link = attr.Split('?')[0];
-                            job.Url = link;
-                            job.QuickApply = false;
-                        }
                         jobs.Add(job);
-   
-
                     }
                 }
 
